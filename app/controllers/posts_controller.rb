d694281e1546390b7c6713458c4cc0aa96961851
dloughborough <david.loughborough@kellogg.northwestern.edu>
2022-02-21 16:54:47 +0000

    further updated posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+      end
+    
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+      end
 end
